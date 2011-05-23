6989cda453de717b5edd362ad24e1883cbab4074
Mike Polan <mbpolan@gmail.com>
2011-05-23 02:32:03 +0000

    -Added empty Protocol class to prepare network communications component of server. -Added Packet class to encapsulate raw packet data send to/from client. -Included missing comment header in configfile.cpp. -Few minor tweaks to the lobbyserver.cpp initialization code.

--- lobby_server/trunk/src/lobbyserver.cpp
+++ lobby_server/trunk/src/lobbyserver.cpp
@@ -25,6 +25,7 @@
 
 #include "configfile.h"
 #include "lobbyserver.h"
+#include "protocol.h"
 #include "serversocket.h"
 
 // globals
---
+++ lobby_server/trunk/src/protocol.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by the Tyranny Development Team                    *
+ *   http://tyranny.sf.net                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+// protocol.h: definition of the Protocol class.
+
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+class Protocol {
+	public:
+		/**
+		 * Default constructor.
+		 * @param socket Socket to associate with this protocol.
+		 */
+		Protocol(int socket);
+
+		/**
+		 * Begins the communication loop with the client.
+		 */
+		void communicationLoop();
+
+	private:
+		/// Socket for reading/writing data between the server and client.
+		int m_Socket;
+};
+
+#endif
