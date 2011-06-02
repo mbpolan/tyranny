acb2ec3bddf241a37e4f14d95c286692020c7b27
Mike Polan <mbpolan@gmail.com>
2011-06-02 00:52:31 +0000

    -Implemented basic authentication by plain text username and password

---
+++ lobby_server/trunk/src/protspec.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by the Tyranny Development Team                    *
+ *   http://tyranny.sf.net                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+// protspec.h: definition of the client-server protocol.
+
+#ifndef PROTSPEC_H
+#define PROTSPEC_H
+
+/// Authentication class packets
+#define AUTH_DATA			0xA0
+#define AUTH_SUCCESS		0xA1
+#define AUTH_ERROR			0xA2
+#define AUTH_LOGOUT			0xA3
+#define AUTH_REQUEST		0xA4
+
+#endif
